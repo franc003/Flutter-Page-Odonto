81646b18bb453cb705d3a0c4e6fc1eace900c94c
franc003 <100725706+franc003@users.noreply.github.com>
2023-05-30 02:32:17 -0300

    Create page02.dart

---
+++ page02.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Page02 extends StatelessWidget {
+  const Page02({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pagina de obras sociales"),
+      ),
+    );
+  }
+}
