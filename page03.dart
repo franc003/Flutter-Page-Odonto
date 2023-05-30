7897b6de38b841e5ea05c3c4641ec94c4a44a941
franc003 <100725706+franc003@users.noreply.github.com>
2023-05-30 02:33:02 -0300

    Create page03.dart

---
+++ page03.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Page03 extends StatelessWidget {
+  const Page03({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pagina Sacar Turno"),
+      ),
+    );
+  }
+}
